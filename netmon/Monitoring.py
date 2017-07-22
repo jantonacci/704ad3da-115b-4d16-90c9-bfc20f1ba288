e9965c7f98a1f520326628892fd32e2f92f79cd7
Jason Antonacci <jantonacci@users.noreply.github.com>
2017-07-22 14:20:07 -0600

    Working local monitoring code plus mockup for remote

--- netmon/Monitoring.py
+++ netmon/Monitoring.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python3
 
 import Probes
+import time
 
 
-class ProbesLocal(object):
+class ProbeExec(object):
     def __init__(self, hosts=['localhost'], probes=[Probes.ScanHost]):
         self.hosts = hosts
         self.probes = probes
         self.results = []
         self.failures = []
+
+    def exec_local(self):
         try:
             for host in self.hosts:
                 for probe in self.probes:
                     probe_results = probe(host=host).results
+                    probe_results["fn"] = "exec_local"
                     self.results.append(probe_results)
                     if probe_results.get('returncode', 0) != 0:
                         self.failures.append(probe_results)
@@ -20,9 +24,26 @@ class ProbesLocal(object):
             # TODO: additional error handling here
             raise Exception(err)
 
+    def exec_remote(self, host='localhost'):
+            # Use self.results[N].get('args') to gather executed commands
+            results = []
+            for item in self.results:
+                args = item.get('args')
+                name = 'Remote%s' % item.get('name')
+                # TODO: Use paramiko (SSH) for remote execution and capture results
+                results.append({"fn": "exec_remote",
+                                "name": name,
+                                "args": args,
+                                "returncode": 127,
+                                "stdout": "paramiko remote exec stdout",
+                                "time": time.time()})
+            self.results.extend(results)
+
 
 if __name__ == "__main__":
-    probes = ProbesLocal(hosts=['0.0.0.0', 'localhost'],
-                         probes=[Probes.ScanHost, Probes.ping_host])
-    for result in probes.failures:
+    probes = ProbeExec(hosts=['0.0.0.0', 'localhost'],
+                       probes=[Probes.ScanHost, Probes.ping_host])
+    probes.exec_local()
+    probes.exec_remote()
+    for result in probes.results:
         print(result)
